f1454fe262501f326278b2c0d7a7f3e35f559430
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2022-10-14 11:30:48 +0200

    clean project

---
+++ src/oplossing/TopDownSemiSplayTwoThreeTree.java
@@ -0,0 +1,42 @@
+package oplossing;
+
+import opgave.SearchTree;
+
+import java.util.Iterator;
+
+public class TopDownSemiSplayTwoThreeTree<E extends Comparable<E>> implements SearchTree<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(E o) {
+        return false;
+    }
+
+    @Override
+    public boolean add(E o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(E e) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
